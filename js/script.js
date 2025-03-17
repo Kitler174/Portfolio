945fe2d857af8d4d6f42a3b2002a5572108ce9a6
Kitler174 <mateuszblaszczyk36@gmail.com>
2025-03-17 12:24:59 +0100

    zmiany

--- js/script.js
+++ js/script.js
@@ -20,3 +20,76 @@ window.addEventListener('load', function() {
         }, index * 500);
     });
 });
+
+var index = 1;
+const b1 = document.getElementById("button1");
+const b2 = document.getElementById("button2");
+function suw(act){
+        var el1 = document.getElementById("projekt"+index)
+        
+        if (act == 2){
+            if (index < 5){
+                index++;
+            }
+            else {
+                b2.style.backgroundColor = "red";
+                b2.style.transform = "rotate(5deg)";
+            
+                setTimeout(() => {
+                    b2.style.transform = "rotate(-5deg)";
+                }, 100);
+            
+                setTimeout(() => {
+                    b2.style.backgroundColor = "#3b5998";
+                    b2.style.transform = "rotate(5deg)";
+                }, 200);
+            
+                setTimeout(() => {
+                    b2.style.transform = "rotate(-5deg)";
+                }, 300);
+                setTimeout(() => {
+                    b2.style.transform = "rotate(0)";
+                }, 300);
+            }
+            var el2 = document.getElementById("projekt"+index)
+            el1.style.transform = "translateX(-120%)"
+            el1.style.opacity = "0"
+            el1.className = "additionals_inact"
+            el2.className = "additionals_"
+            el2.style.transform = "translateX(0)"
+            el2.style.opacity = "1"
+        }     
+        else{
+            if (index > 1){
+                index--;
+            }
+            else {
+                b1.style.backgroundColor = "red";
+                b1.style.transform = "rotate(5deg)";
+            
+                setTimeout(() => {
+                    b1.style.transform = "rotate(-5deg)";
+                }, 100);
+            
+                setTimeout(() => {
+                    b1.style.backgroundColor = "#3b5998";
+                    b1.style.transform = "rotate(5deg)";
+                }, 200);
+            
+                setTimeout(() => {
+                    b1.style.transform = "rotate(-5deg)";
+                }, 300);
+                setTimeout(() => {
+                    b1.style.transform = "rotate(0)";
+                }, 300);
+            }
+            
+            var el2 = document.getElementById("projekt"+index)
+            el1.style.transform = "translateX(120%)"
+            el1.style.opacity = "0"
+            el1.className = "additionals_inact"
+            el2.className = "additionals_"
+            el2.style.transform = "translateX(0)"
+            el2.style.opacity = "1"
+        }
+}
